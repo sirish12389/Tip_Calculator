28d6c1f14fb086939a9261d65911c024cc503739
sirish12389 <sirish12389@gmail.com>
2021-08-14 12:15:09 +0530

    completed css

---
+++ JavaScript/Button.js
@@ -0,0 +1,12 @@
+function keep_button_active(id){
+    var button = document.getElementById(id);
+    var current = document.getElementsByClassName("active");
+    if(current.length >= 1)
+        current[0].className = "btn";
+    if(button.id != "txt_cust_tip")
+        button.className = "btn active";
+}
+
+function alert_hi(){
+    alert("Hi");
+}
